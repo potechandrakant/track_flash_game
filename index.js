9983c7ce8dd214600112fa9e3ab5c86ec983bd52
Chandrakant Pote <chandrakantpote132@gmail.com>
2025-02-27 23:33:52 +0530

    changes

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.set("views engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+
+app.get("/login", (req,res) => {
+    res.send("Success");
+})
+
+
+app.listen("8080", ()=> {
+    console.log("App is working");
+})
