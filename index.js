6e2d7649c65cfabde757d29adb978b630e62a86e
Chandrakant Pote <chandrakantpote132@gmail.com>
2025-02-28 18:16:18 +0530

    changes

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
-const path = require("path");
 const app = express();
+const path = require("path");
+const mysql = require("mysql2");
 
 app.set("views engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "game",
+    password: "Pote_1234"
+});
 
-app.get("/login", (req,res) => {
+app.get("/register", (req,res)=>{
+    res.render("login.ejs");   
+})
+
+app.post("/register", (req,res) => {
+    let {userName,userEmail,userPassword} = req.body;
     res.send("Success");
 });
 
@@ -14,3 +26,5 @@ app.get("/login", (req,res) => {
 app.listen("8080", ()=> {
     console.log("App is working");
 })
+
+
