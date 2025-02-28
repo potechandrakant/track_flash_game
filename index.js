f7e9b42f4a2f2b5e1b9fedcbfbda436ef76311f4
Chandrakant Pote <chandrakantpote132@gmail.com>
2025-03-01 01:49:06 +0530

    login

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     database: "game",
-    password: "mahesh@2005"
+    password: "Pote_1234"
 });
 
 app.get("/", (req,res)=>{
@@ -36,6 +36,29 @@ app.post("/register",(req,res)=>{
         res.send("some error in DB")
     }
 })
+
+app.post("/login", (req, res) => {
+    let { loginName } = req.body;
+    let { loginPassword } = req.body;
+
+    let q = `SELECT * FROM user WHERE username='${loginName}'`;
+
+    connection.query(q, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.send("<script>alert('Internal Server Error'); window.location.href = '/';</script>");
+            return;
+        }
+
+        if (result.length > 0 && loginName === result[0].username && loginPassword === result[0].userpass) {
+            res.send("<script>alert('Login successful!'); window.location.href = '/';</script>");
+            console.log(result[0].username);
+        } else {
+            res.send("<script>alert('Login failed: Incorrect username and password or no user found.'); window.location.href = '/';</script>");
+            console.log("Login failed: Incorrect username and password or no user found.");
+        }
+    });
+});
 
 
 
