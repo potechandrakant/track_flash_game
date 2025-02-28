9e7b6a55fbd760ada0490176381f776a396cd784
Mahesh Wankhede <113539413+Mahesh0925@users.noreply.github.com>
2025-02-28 09:24:14 -0800

    register data

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const path = require("path");
 const mysql = require("mysql2");
 
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
 app.set("views engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -11,17 +13,30 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     database: "game",
-    password: "Pote_1234"
+    password: "mahesh@2005"
 });
 
-app.get("/register", (req,res)=>{
-    res.render("login.ejs");   
+app.get("/", (req,res)=>{
+    res.render("home.ejs");   
 })
 
-app.post("/register", (req,res) => {
-    let {userName,userEmail,userPassword} = req.body;
-    res.send("Success");
-});
+app.post("/register",(req,res)=>{
+    let {userName , userEmail , userPassword} = req.body;
+    console.log(userName);
+
+    let q="INSERT INTO user (username,email,userpass) VALUES (?,?,?)"
+    try {
+        connection.query(q,[userName,userEmail,userPassword], (err, result) => {
+            if (err) throw err;
+            console.log(result);
+            res.redirect("/")
+        });
+    } catch (err) {
+        console.log(err);
+        res.send("some error in DB")
+    }
+})
+
 
 
 app.listen("8080", ()=> {
